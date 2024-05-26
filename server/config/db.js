51ab3ceb192407ca9497b6c18c32db7471aa5da0
riseMediaTeam <rise.shopmarket@gmail.com>
2024-05-26 14:18:01 +0300

    s

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const connectDB = async () => {
-  
   try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    mongoose.set("strictQuery", false);
+
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true,
+      tlsAllowInvalidCertificates: true, // Adjust based on your requirements
+    });
+
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log("Error connecting to MongoDB:", error.message);
   }
-
-}
+};
 
 module.exports = connectDB;
